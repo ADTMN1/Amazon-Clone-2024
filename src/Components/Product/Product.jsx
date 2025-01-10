aa0504de2a5b224fe19c8a48b58609c0a5f588c2
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-10 12:19:53 -0800

    Loading Functionality Integration

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,28 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProductCard from './ProductCard';
-import classes from './Product.module.css'
+import classes from './Product.module.css';
+import Loader from '../Loader/Loader';
+
 function Product() {
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
+        setIsLoading(true);
         axios.get('https://fakestoreapi.com/products')
             .then((res) => {
-                setProducts(res.data)
-            }).catch((err) => {
-                console.log(err)
+                setProducts(res.data);
+                setIsLoading(false);
             })
-    }, [])
-    return (
-        <section className={classes.product_container}>
-            {
-                products.map((singleProduct) => {
-                    return <ProductCard product={singleProduct} key={singleProduct.id} />
-                })
-            }
+            .catch((err) => {
+                console.error(err);
+                setIsLoading(false);
+            });
+    }, []);
 
-        </section>
-    )
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <section className={classes.product_container}>
+                    {products?.map((singleProduct) => {
+                        return <ProductCard product={singleProduct} key={singleProduct.id} />
+                    })}
+                </section>
+            )}
+        </>
+    );
 }
 
-
-export default Product
+export default Product;
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -4,22 +4,28 @@ import { useParams } from 'react-router-dom'
 import { productUrl } from '../../Api/endPoints'
 import axios from 'axios'
 import ProductCard from '../../Components/Product/ProductCard'
+import Loader from '../../Components/Loader/Loader'
 function ProductDetail() {
     const { productId } = useParams()
     const [product, setProduct] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
+        setIsLoading(true);
         axios.get((`${productUrl}/products/${productId}`))
             .then((res) => {
-                setProduct(res.data)
+                setProduct(res.data);
+                setIsLoading(false);
             }).catch((err) => {
-                console.log(err)
+                console.log(err);
+                setIsLoading(false);
             })
 
     }, []);
     return (
         <LayOut>
-            <ProductCard
-                product={product} />
+            {isLoading ? (<Loader />) : (<ProductCard
+                product={product} />)}
+
         </LayOut>
     )
 }
