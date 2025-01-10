739ab4fb983ba90305ebb21e4300dd1a9e30c3bc
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-10 09:06:13 -0800

    Category Routing

--- src/Components/Catagory/CategoryFullinfos.jsx
+++ src/Components/Catagory/CategoryFullinfos.jsx
@@ -1,26 +1,26 @@
 export const CategoryInfos = [
 
     {
-        title: "Home Essential",
-        name: "home essential",
-        imgLink: "https://m.media-amazon.com/images/I/81Ehj3jarwL._AC_UL480_FMwebp_QL65_.jpg"
-
+        title: "Electronics",
+        name: "electronics",
+        imgLink: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg'
+    },
+    {
+        title: "Discover Fashion Trends",
+        name: "women's clothing",
+        imgLink: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
 
     },
     {
-        title: "Get your game on",
-        name: "get your game on",
-        imgLink: "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2024/Stores-Gaming/FinalGraphics/Fuji_Gaming_store_Dashboard_card_2x_EN._SY608_CB564799420_.jpg"
+        title: "Jewelery",
+        name: "jewelery",
+        imgLink: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg'
+
     },
     {
-        title: "Men's Fashion",
-        name: "men's fashion",
+        title: "Men's Clothing",
+        name: "men's clothing",
         imgLink: "https://m.media-amazon.com/images/I/81sn3PmEmrL._AC_UL480_FMwebp_QL65_.jpg"
-    },
-    {
-        title: "Drone's Collection",
-        name: "Drone",
-        imgLink: "https://m.media-amazon.com/images/I/61fTOI05WlL._AC_UL480_FMwebp_QL65_.jpg"
     },
 
 ]
