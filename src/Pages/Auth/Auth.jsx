7cbf11210dabd147c551c2201c2a75c082101dd5
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-14 07:52:45 -0800

    Authentication Functionality

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import classes from './SignIn.module.css';
+import { auth } from '../../Utility/fireBase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { DataContext } from '../../Components/DataProvider/DataProvider'
+import { Type } from '../../Utility/action.type';
 function Auth() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const [{ user }, dispatch] = useContext(DataContext);
+
+    const authHandler = async (e) => {
+        e.preventDefault();
+
+        if (e.target.name == 'signin') {
+            signInWithEmailAndPassword(auth, email, password).then((userInfo) => {
+                dispatch({
+                    type: Type.SET_USER,
+                    user: userInfo.user
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
+
+        } else {
+            createUserWithEmailAndPassword(auth, email, password).then((userInfo) => {
+                dispatch({
+                    type: Type.SET_USER,
+                    user: userInfo.user
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    };
+
     return (
         <section className={classes.login}>
             <Link><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Amazon_2024.svg/180px-Amazon_2024.svg.png' alt="" /></Link>
@@ -11,18 +47,18 @@ function Auth() {
 
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id='email' />
+                        <input value={email} onChange={((e) => setEmail(e.target.value))} type="email" id='email' />
 
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="password" id='password' />
+                        <input value={password} onChange={((e) => setPassword(e.target.value))} type="password" id='password' />
                     </div>
-                    <button className={classes.login_signInButton}>Sign In</button>
+                    <button name='signin' type="submit" onClick={authHandler} className={classes.login_signInButton}>Sign In</button>
 
                 </form>
                 <p>By signing-in you agree to the AMAZON FAKE CLONE conditions of Use & Sale. Please see our Privacy Notice. Our Cookies Notice and our Interest-Based Ads Notice. </p>
-                <button className={classes.login_registerButton}>Create Your Amazon Account</button>
+                <button name='signup' type="submit" onClick={authHandler} className={classes.login_registerButton}>Create Your Amazon Account</button>
             </div>
         </section>
     )
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -1,7 +1,8 @@
 import { Type } from './action.type'
 
 export const initialState = {
-    basket: []
+    basket: [],
+    user: null
 }
 export const reducer = (state, action) => {
     switch (action.type) {
@@ -35,6 +36,12 @@ export const reducer = (state, action) => {
 
             return {
                 ...state, basket: newBasket
+            }
+
+        case Type.SET_USER:
+            return {
+                ...state,
+                user: action.user
             }
 
         default:
