082a705dca774220b557efdb1a4fb8af079cf2b1
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-23 05:50:43 -0800

    Client side payment functionality

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,35 @@
-
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config()
-const stripe = require("stripe")(process.env.STRIPE_KEY);
-
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const app = express();
-app.use(cors({ origin: true }))
-
-app.use(express.json())
-
-app.get("/", (req, res) => {
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get('/', (req, res) => {
     res.status(200).json({
-        message: "success"
-    })
-})
+        message: 'Success!',
+    });
+});
 
-app.post("/payment/create", async (req, res) => {
-    const total = req.query.total;
+app.post('/payment/create', async (req, res) => {
+    const amount = req.query.total;
 
-    if (total > 0) {
+    if (amount > 0) {
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: total,
-            currency: "usd"
-        })
-
-        res.status(201).json({
-            clientSecret: paymentIntent.client_secret
-        })
-
-    } else {
-        res.status(403).json({
-            message: "total must be greater than 0"
+            amount,
+            currency: 'usd',
         });
 
+        res.status(201).json({ clientSecret: paymentIntent.client_secret });
+    } else {
+        res.status(403).json({
+            message: 'Amount Must be greater than zero',
+        });
     }
-})
+});
 
-exports.api = onRequest(app)
+exports.api = onRequest(app);
--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,41 +1,75 @@
 import React, { useContext, useState } from 'react';
-import LayOut from '../../Components/LayOut/LayOut'
-import classes from "./Payment.module.css"
+import LayOut from '../../Components/LayOut/LayOut';
+import classes from "./Payment.module.css";
 import { DataContext } from '../../Components/DataProvider/DataProvider';
 import ProductCard from '../../Components/Product/ProductCard';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import CurrenceFormat from '../../Components/CurrenceFormat/CurrenceFormat';
+import { axiosInstance } from '../../Api/Axios';
+import { ClipLoader } from 'react-spinners'
+import { db } from '../../Utility/fireBase';
+import { useNavigate } from 'react-router-dom';
 
 function Payment() {
-    const [{ user, basket }, dispatch] = useContext(DataContext)
-    const totalItem = basket?.reduce((amount, item) => {
-        return item.amount + amount
-    }, 0)
-
-    const total = basket.reduce((amount, item) => {
-        return item.price * item.amount + amount
-    }, 0)
-
-    const [cardError, setcardError] = useState(null)
+    const [{ user, basket }] = useContext(DataContext);
+    const totalItem = basket?.reduce((amount, item) => item.amount + amount, 0);
+    const total = basket.reduce((amount, item) => item.price * item.amount + amount, 0);
+    const [cardError, setCardError] = useState(null);
     const stripe = useStripe();
     const elements = useElements();
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
-    const handlechange = (e) => {
-        e?.error?.message ? setcardError(e?.error?.message) : setcardError("")
+    const handleChange = (e) => {
+        e?.error?.message ? setCardError(e?.error?.message) : setCardError("");
+    };
+    const handlePayment = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        try {
+            const response = await axiosInstance({
+                method: "POST",
+                url: `/payment/create?total=${total * 100}`,
+            });
+            const clientSecret = response?.data?.clientSecret;
+
+            if (!clientSecret) {
+                setIsLoading(false);
+                return;
+            }
+            const { paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                },
+            });
+            if (paymentIntent.error) {
+                setIsLoading(false);
+            } else {
+                setIsLoading(false);
+            }
+
+            await db.collection("users").doc(user.uid).collection("orders").doc(paymentIntent.id).set({
+                basket: basket,
+                amount: paymentIntent.amount,
+                created: paymentIntent.created,
+            })
+            navigate("/orders", { state: { msg: "you have placed new order" } })
+        } catch (error) {
+            setIsLoading(false);
+        }
     }
-
     return (
         <LayOut>
             <div className={classes.payment_header}>
                 Checkout ({totalItem}) items
             </div>
             <section className={classes.payment}>
-
                 <div className={classes.flex}>
-                    <h3>  Delivery Address</h3>
+                    <h3>Delivery Address</h3>
                     <div>
                         <h3>{user?.email}</h3>
-                        <h3>bole,01 street</h3>
+                        <h3>bole, 01 street</h3>
                         <h3>Addis Ababa</h3>
                     </div>
                 </div>
@@ -45,40 +79,42 @@ function Payment() {
                     <div>
                         {basket?.map((item) => (
                             <ProductCard key={item.id} product={item} flex={true} />
-                        ))};
-
+                        ))}
                     </div>
                 </div>
                 <hr />
-
                 <div className={classes.flex}>
                     <h3>Payment method</h3>
-
                     <div className={classes.payment_card_container}>
                         <div className={classes.payment__details}>
-                            <form action="">
+                            <form onSubmit={handlePayment}>
                                 {cardError && <small style={{ color: "red" }}>{cardError}</small>}
-                                <CardElement onChange={
-                                    handlechange
-                                } />
+                                <CardElement onChange={handleChange} />
                                 <div className={classes.payment__price}>
                                     <div>
                                         <span style={{ display: "flex", gap: "10px" }}>
-                                            <p> Total Order |</p> <CurrenceFormat amount={total} />
+                                            <p>Total Order |</p> <CurrenceFormat amount={total} />
                                         </span>
                                     </div>
-                                    <button>Pay Now</button>
+                                    <button type='submit' disabled={!stripe}>
+                                        {
+                                            isLoading ? (
+                                                <div className={classes.loading}>
+                                                    <ClipLoader color='#000' size={15} />
+                                                    <p>Please wait...</p>
+                                                </div>) :
+                                                "Pay Now"
+                                        }
+
+                                    </button>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
-
-
             </section>
-
-        </LayOut >
-    )
+        </LayOut>
+    );
 }
 
-export default Payment
+export default Payment;
