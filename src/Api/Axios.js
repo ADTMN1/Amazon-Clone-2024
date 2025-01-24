dbc2ed1b02856e9aa4c91a3323532563201b25e3
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-24 06:00:03 -0800

    update responsiveness

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-3c260/us-central1/api",
+    //local instance of firebase function
+
+    // baseURL: "http://127.0.0.1:5001/clone-3c260/us-central1/api",
+
+    //deployed version of amazon server on render.com
+    baseURL: "https://amazon-api-deploy-fzpx.onrender.com",
 })
 
 export { axiosInstance }
